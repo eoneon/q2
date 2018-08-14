537d1651d3087ef17d62b1f3660c065a1fc7f5a4
Eon <eoneon123@gmail.com>
2018-08-13 21:25:04 -0700

    fixed properties loop work inside category_fields loop upon the condition of a item_field haveing no associated child field_values

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,30 +27,35 @@ class Item < ApplicationRecord
   end
 
   def mounting_type
-    h = {"framed" => "frame_kind, frame_width, frame_height", "wrapped" => "wrapped_kind", "border" =>"border_kind, border_width, border_height"}
+    h = {"framed" => "frame_kind frame_width frame_height", "wrapped" => "wrapped_kind", "border" =>"border_kind border_width border_height"}
   end
 
   def substrate_type
-    h = {"canvas" => "canvas_kind, image_width, image_height", "paper" => "paper_kind, image_width, image_height", "board" => "board_kind, image_width, image_height"}
+    h = {"canvas" => "canvas_kind image_width image_height", "paper" => "paper_kind image_width image_height", "board" => "board_kind image_width image_height"}
   end
 
   def edition_type
-    h = {"numbered_xy" => "edition_kind, edition_number, edition_size", "from_an_edition" => "edition_kind", "roman_numbered" => "edition_kind, roman_number, roman_size"}
+    h = {"numbered_xy" => "edition_kind edition_number edition_size", "from_an_edition" => "edition_kind", "roman_numbered" => "edition_kind roman_number roman_size"}
   end
 
   def category_fields
     fields = category.fields_arr
     %w(mounting_type substrate_type edition_type).each do |type_field|
-      if parent_type_value(type_field)[0].present?
+      if parent_type_value(type_field)[0].present? && parent_type_value(type_field)[0] != "numbered_qty"
         idx = fields.index(type_field) + 1
-        new_fields = public_send(type_field)[parent_type_value(type_field)[0]].split(",")
+        new_fields = public_send(type_field)[parent_type_value(type_field)[0]].squish.split(" ")
         fields.insert(idx, new_fields)
       end
     end
     fields.flatten.map {|field_name| ItemField.where(field_name: field_name)}.flatten
+    #fields.flatten
   end
 
   def selected_field_value(item_field)
     field_value_ids & item_field.field_value_ids
+  end
+
+  def wtf
+    ItemField.where(field_name: "edition_number")
   end
 end
