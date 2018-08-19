329b1dc729ecd6b89c7449741c7452d24a55bef3
Eon <eoneon123@gmail.com>
2018-08-18 17:42:36 -0700

    still can't remove association

--- app/controllers/category_fields_controller.rb
+++ app/controllers/category_fields_controller.rb
@@ -8,9 +8,9 @@ class CategoryFieldsController < ApplicationController
   #   @item_field = ItemField.new
   # end
 
-  def edit
-    @item_field = ItemField.find(params[:id])
-  end
+  # def edit
+  #   @item_field = ItemField.find(params[:id])
+  # end
 
   def create
     @category = Category.find(params[:category_id])
@@ -25,23 +25,39 @@ class CategoryFieldsController < ApplicationController
     else
       flash.now[:alert] = "Error creating ItemField. Please try again."
     end
-    render :new
+    redirect_to @category
   end
 
-  def update
-    @item_field = ItemField.find(params[:id])
-    @item_field.assign_attributes(item_field_params)
+  # def update
+  #   @item_field = ItemField.find(params[:id])
+  #   @item_field.assign_attributes(item_field_params)
+  #
+  #   if @item_field.save
+  #     flash[:notice] = "item_field was updated successfully."
+  #     redirect_to action: :index
+  #   else
+  #     flash.now[:alert] = "Error updated item_field. Please try again."
+  #     render :edit
+  #   end
+  # end
 
-    if @item_field.save
-      flash[:notice] = "item_field was updated successfully."
-      redirect_to action: :index
+  def destroy
+    @category = Category.find(params[:category_id])
+    field_group = @category.field_groups.build(category_params)
+    #field_group = FieldGroup.find(field_group.id)
+    #item_field = ItemField.find(params[:id])
+    #field_group = FieldGroup.where(category_id: @category.id, item_field_id: item_field.id)
+
+    if field_group.destroy
+      flash[:notice] = "field_group #{field_group.item_field_id} - #{field_group.category_id} was deleted successfully."
+      redirect_to @category
     else
-      flash.now[:alert] = "Error updated item_field. Please try again."
-      render :edit
+      flash.now[:alert] = "There was an error deleting the field_group."
+      redirect_to @category
     end
   end
 
-  def item_field_params
-    params.require(:item_field).permit!
+  def category_params
+    params.require(:field_group).permit!
   end
 end
